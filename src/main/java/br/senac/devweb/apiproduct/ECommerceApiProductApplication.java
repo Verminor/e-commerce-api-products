1db8924a847602826a5c290afd98e0cf42f0a11f
Verminor <Thrazejin@gmail.com>
2021-09-10 21:00:16 -0300

    setup inicial

---
+++ src/main/java/br/senac/devweb/apiproduct/ECommerceApiProductApplication.java
@@ -0,0 +1,13 @@
+package br.senac.devweb.apiproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApiProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/devweb/apiproduct/ECommerceApiProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.devweb.apiproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
